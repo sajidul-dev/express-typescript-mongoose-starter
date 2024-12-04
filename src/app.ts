eb8084c7bbefa57a3544d6e952974ccebd81b136
sajidul-dev <mdsajidulalam1245@gmail.com>
2024-12-04 13:48:52 +0600

    [Done]: Updated readme

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-import express, { Request, Response } from 'express';
-import { Application } from 'express';
-import cors from 'cors';
+import express, { Request, Response } from "express";
+import { Application } from "express";
+import cors from "cors";
 const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
 });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
-import mongoose from 'mongoose';
-import app from './app';
-import config from './config';
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
 
 async function main() {
   try {
-    console.log('Connecting....');
+    console.log("Connecting....");
     await mongoose.connect(config.database_url as string);
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
 
     app.listen(config.port, () => {
       console.log(`App is listening to port ${config.port}`);
     });
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
+    console.error("Error connecting to MongoDB:", error);
   }
 }
 main();
